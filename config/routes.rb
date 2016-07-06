74e9a2df6844c55ef1108dac580fd1b1ba17bee8
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-06 08:42:03 -0400

    Moved routes back into api subdomain.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Jobberwocky::Application.routes.draw do
   match "*all", to: "application#preflight", via: [:options]
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    devise_for :users, :controllers => {
+      omniauth_callbacks: "omniauth_callbacks",
+      sessions: 'sessions', registrations: 'registrations'
+    }
 
-  devise_for :users, :controllers => {
-    omniauth_callbacks: "omniauth_callbacks",
-    sessions: 'sessions', registrations: 'registrations'
-  }
+    resources :users
+  end
 end

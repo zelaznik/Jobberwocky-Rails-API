0cc3b5eb8fc5a99eb8906bd8986b05db7d2661d4
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-06 20:39:06 -0400

    The callback_uri wasn't populating, which was casuing problems. This should fix it.

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,26 +6,32 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   respond_to :html, only: []
   respond_to :xml, only: []
 
-  def self.provides_callback_for(provider)
-    define_method provider do
-      callback_uri = params[:callback_uri]
-      user = User.find_for_oauth(env["omniauth.auth"], current_user)
-      if user.persisted?
-        # sign_in user, event: :authentication
-        user.generate_authentication_token!
-        auth = {
-          auth_token: user.auth_token,
-          email: user.email, id: user.id
-        }
-      else
-        auth = {user: false}
-      end
-
-      redirect_to "#{callback_uri}?#{auth.to_query}"
+  def provides_callback_for(provider)
+    callback_uri = params[:callback_uri]
+    user = User.find_for_oauth(env["omniauth.auth"], current_user)
+    if user.persisted?
+      # sign_in user, event: :authentication
+      user.generate_authentication_token!
+      auth = {
+        auth_token: user.auth_token,
+        email: user.email, id: user.id
+      }
+    else
+      auth = {user: false}
     end
+
+    redirect_to "#{callback_uri}?#{auth.to_query}"
   end
 
-  [:twitter, :facebook].each do |provider|
-    provides_callback_for provider
+  def twitter
+    provides_callback_for :twitter
+  end
+
+  def facebook
+    provides_callback_for :facebook
+  end
+
+  def auth_params
+    params.permit(:callback_url)
   end
 end

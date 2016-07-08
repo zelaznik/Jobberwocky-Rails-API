28fd9f9a5fa0cddf5c8fd2ae184a951810329710
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-08 04:15:48 -0400

    Made the email/password requirement more flexible with omniauth.

--- db/migrate/20160706055136_devise_create_users.rb
+++ db/migrate/20160706055136_devise_create_users.rb
@@ -1,9 +1,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
-      ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      ## Database authenticatable, unless omniauthable
+      t.string :email
+      t.string :encrypted_password
 
       ## Recoverable
       t.string   :reset_password_token

fc5281d333a4dcfdc1fbc2b77e1d2a78eba7aa66
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-11 01:39:53 -0400

    Renders 404 in json.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,8 @@
 class ErrorsController < ApplicationController
   skip_before_filter :authenticate_request
   def not_found
-    render json: {error: "Route not found"}, status: 404
+    data = {}
+    data[request.method] = request.url
+    render json: data, status: 404
   end
 end

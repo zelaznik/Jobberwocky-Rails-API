f65fc7f5c7d39729f6b8e7acf7e6df4331579905
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-11 12:45:14 -0400

    ALlowed CORS for all paths.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::Base
   end
 
   def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || ""
+    headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Allow-Headers'] = 'Origin,Content-Type,X-Auth-Token,Authorization'
     headers['Access-Control-Max-Age'] = "1728000"

aa2261ec6a24847a0355ef680c7e56b32ac2e161
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-13 09:26:49 -0400

    Made pusher_auth a GET route only.

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Jobberwocky::Application.routes.draw do
       resources :messages, only: [:index, :create]
     end
 
-    match "pusher_auth/:token", to: "pusher#auth", via: :all
+    get "pusher_auth/:token", to: "pusher#auth"
     match "*all", to: "errors#not_found", via: :all
   end
 end

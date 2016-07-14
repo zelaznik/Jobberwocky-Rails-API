5f6fceb05ef9ffec1ab61a4c92be8ba72fefa170
Steve Zelaznik <zelaznik@yahoo.com>
2016-07-14 11:31:00 -0400

    Got rid of N+1 for users#index.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:index, :show]
 
   def index
-    respond_with User.all, status: 200
+    respond_with User.includes(:identities).all, status: 200
   end
 
   def show
